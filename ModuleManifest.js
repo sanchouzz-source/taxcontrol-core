ddb22efcd4621fe02666705df93991a9b15f70d5
Aleksander <sanchouzz@gmail.com>
2026-07-13 07:43:44 +0300

    создал modulemanifest

---
+++ ModuleManifest.js
@@ -0,0 +1,23 @@
+const ModuleManifest = [
+
+{
+    name:"FinanceEngine",
+    priority:20
+},
+
+{
+    name:"KPIEngine",
+    priority:30
+},
+
+{
+    name:"DashboardEngine",
+    priority:40
+},
+
+{
+    name:"TripEventHandler",
+    priority:10
+}
+
+];
